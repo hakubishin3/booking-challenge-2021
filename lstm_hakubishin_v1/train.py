885add1b535b6e7cb60726ac26bd1c0d1bfcd0c1
hakubishin3 <shu@wantedly.com>
2021-01-09 14:09:50 +0000

    Add preprocessing process in train.py

--- lstm_hakubishin_v1/train.py
+++ lstm_hakubishin_v1/train.py
@@ -1,7 +1,11 @@
 import yaml
 import torch
 import argparse
+import pandas as pd
 from pathlib import Path
+from sklearn import preprocessing
+from sklearn.model_selection import StratifiedKFold
+
 from src import log, set_out, span, load_train_test_set
 from src.utils import seed_everything
 
@@ -18,7 +22,35 @@ def run(config: dict) -> None:
     train_test_set = load_train_test_set(config)
     log(f"{train_test_set.shape}")
 
-    import pdb; pdb.set_trace()
+    log("Preprocessing:")
+    target_le = preprocessing.LabelEncoder()
+    train_test_set["city_id"] = target_le.fit_transform(train_test_set["city_id"])
+
+    train = train_test_set[train_test_set["row_num"].isnull()]
+    test = train_test_set[~train_test_set["row_num"].isnull()]
+
+    train_trips = train.groupby("utrip_id")["city_id"].apply(list)
+    test_trips = test.query("city_id!=0").groupby("utrip_id")["city_id"].apply(list)
+
+    x_train = pd.DataFrame(train_trips)
+    x_test = pd.DataFrame(test_trips)
+
+    x_train["n_trips"] = x_train["city_id"].map(lambda x: len(x))
+    x_train = x_train.query("n_trips > 2").sort_values("n_trips").reset_index(drop=True)
+    x_test = x_test.reset_index(drop=True)
+    log(f"x_train: {x_train.shape}, x_test: {x_test.shape}")
+
+    log("Get folds:")
+    cv = StratifiedKFold(
+        n_splits=config["fold"]["n_splits"],
+        shuffle=config["fold"]["shuffle"],
+        random_state=config["seed"],
+    )
+    folds = cv.split(x_train, pd.cut(x_train["n_trips"], 5, labels=False))
+
+    import pdb
+
+    pdb.set_trace()
 
 
 def main():
