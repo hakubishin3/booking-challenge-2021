8bf7509b6b497c35ab60710eb328a948cb6b22ea
hakubishin3 <shu@wantedly.com>
2021-01-09 13:41:58 +0000

    Output the results to the output directory

--- lstm_hakubishin_v1/src/utils.py
+++ lstm_hakubishin_v1/src/utils.py
@@ -8,8 +8,11 @@ def seed_everything(seed: int = 0, gpu_mode: bool = False) -> None:
     random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
     np.random.seed(seed)
-
+    # Torch
     if gpu_mode:
         torch.manual_seed(seed)
         torch.cuda.manual_seed(seed)
         torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
--- lstm_hakubishin_v1/train.py
+++ lstm_hakubishin_v1/train.py
@@ -1,13 +1,23 @@
 import yaml
+import torch
 import argparse
+from pathlib import Path
 from src import log, set_out, span, load_train_test_set
 from src.utils import seed_everything
 
 
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
 def run(config: dict) -> None:
-    seed_everything(71)
+    log("Run with configuration:")
+    log(f"{config}")
+    seed_everything(config["seed"])
+
+    log("Load train and test set:")
     train_test_set = load_train_test_set(config)
-    print(train_test_set.shape)
+    log(f"{train_test_set.shape}")
+
     import pdb; pdb.set_trace()
 
 
@@ -17,6 +27,12 @@ def main():
     args = parser.parse_args()
     with open(args.config, "r") as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
+
+    output_path = Path(config["output_dir_path"]) / config["exp_name"]
+    if not output_path.exists():
+        output_path.mkdir(parents=True)
+    set_out(output_path / "train_log.txt")
+
     run(config)
 
 
