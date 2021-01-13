b727c6052572f2011624e642218269ab2298bcad
hakubishin3 <shu@wantedly.com>
2021-01-13 19:54:56 +0000

    Add FocalLossWithOneHot

--- lstm_hakubishin_v1/src/losses.py
+++ lstm_hakubishin_v1/src/losses.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
+from torch.nn.modules.loss import _Loss
+from pytorch_toolbelt.losses.functional import sigmoid_focal_loss
 
 
 class LabelSmoothingLoss(nn.Module):
@@ -25,3 +27,50 @@ class LabelSmoothingLoss(nn.Module):
         return F.kl_div(logp, target_one_hot, reduction='sum')
 
 
+def one_hot(index, classes):
+    size = index.size() + (classes,)
+    view = index.size() + (1,)
+
+    mask = torch.Tensor(*size).fill_(0)
+    index = index.view(*view)
+    ones = 1.
+
+    print(index)
+    return mask.scatter_(1, index, ones)
+
+
+# https://github.com/DingKe/pytorch_workplace/blob/master/focalloss/loss.py
+class FocalLossWithOneHot(nn.Module):
+    def __init__(self, gamma=0, eps=1e-7):
+        super(FocalLossWithOneHot, self).__init__()
+        self.gamma = gamma
+        self.eps = eps
+
+    def forward(self, input, target):
+        y = one_hot(target, input.size(-1))
+
+        logit = F.softmax(input, dim=-1)
+        logit = logit.clamp(self.eps, 1. - self.eps)
+
+        loss = -1 * y * torch.log(logit) # cross entropy
+        loss = loss * (1 - logit) ** self.gamma # focal loss
+
+        return loss.sum()
+
+
+class FocalLossWithOutOneHot(nn.Module):
+    def __init__(self, gamma=0, eps=1e-7):
+        super(FocalLossWithOutOneHot, self).__init__()
+        self.gamma = gamma
+        self.eps = eps
+
+    def forward(self, input, target):
+        logit = F.softmax(input, dim=1)
+        logit = logit.clamp(self.eps, 1. - self.eps)
+        logit_ls = torch.log(logit)
+        loss = F.nll_loss(logit_ls, target, reduction="none")
+        view = target.size() + (1,)
+        index = target.view(*view)
+        loss = loss * (1 - logit.gather(1, index).squeeze(1)) ** self.gamma # focal loss
+
+        return loss.sum()
